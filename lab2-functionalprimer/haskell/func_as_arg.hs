c8bbe389e576701a52911c00fe55c907eba3adc1
George Hotten <github@ghott.me>
2025-05-01 14:45:51 +0000

    lab 2

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,8 +1,12 @@
 --create inpFunc
-inpFunc = [1..5] 
+inpFunc = [a..b] 
+a = 1
+b = 10
 
 --Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+--applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+applicatorFunc inpFunc s | s == 's' = sum inpFunc
+                         | s == 'a' = (sum inpFunc)/5
 
 main = do
     let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
--- lab2-functionalprimer/haskell/task3.hs
+++ lab2-functionalprimer/haskell/task3.hs
@@ -1,7 +1,10 @@
 sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
+sgnNew x | x < 0 = -1
+         | x == 0 = 0
+         | x > 0 = 1
 
 main = do
     putStrLn "Please enter a number"
     input <- getLine 
     let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
+    putStrLn( show (sgnNew (x)) )
